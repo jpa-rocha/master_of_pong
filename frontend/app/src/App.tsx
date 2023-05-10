6792c84ca53b8a0ab73c08898533d880d45a13e2
jkarosas <jkarosas@student.42wolfsburg.de>
2023-05-10 12:11:19 +0200

    clened up the code a bit

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -41,92 +41,80 @@ import { makeStyles } from '@mui/styles'; */
 
 const App: React.FunctionComponent = () => {
 	
-  return (
-	<Box sx={{ display: 'flex', flexDirection: 'column',  alignItems: 'center', justifyContent: 'center', height: '100vh',
-		margin:0, padding:0}}
-	>
-	<Grid container>
+	return (
+		<Box sx={{ display: 'flex', flexDirection: 'column',  alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+			<Grid container>
 
-{/* 	This is navigation */}
-	  <Grid item xs={12} >
-		 <Box > 
-			<AppBar position="fixed"
- 					style={{ backgroundColor: "#09090C", height:'6vh' }}>
-  				<Toolbar>
-					<Grid container justifyContent="center" alignItems="center">
-					<Grid item xs={3} >
-					
-						 <Box  component="img" alt="Logo" src={logo} sx={{height: 57}}>
-						 </Box>
-						 
-	  					</Grid>
-	  					<Grid item xs={3} >
-						  <Button variant="contained"
-                				sx={{ background: 'linear-gradient(to right, #EA4224 0%, #EDC24F 50%, #EA4224 100%)',
-                  						color: '#FFFFFF'}}>
-							Main
-              			</Button>
-	  					</Grid>
-	  				<Grid item xs={3} >
-						<h2>Master of Pong</h2>
-	  				</Grid>
-	  				<Grid item xs={3} >
-					  	<Button variant="contained"
-                		sx={{background: 'linear-gradient(to right, #EA4224 0%, #EDC24F 50%, #EA4224 100%)',
-                  			color: '#FFFFFF'}}>
-                			Login
-              			</Button>
-	  				</Grid>
-					</Grid>
-  				</Toolbar>
-			</AppBar>
-		</Box> 
-	  </Grid>
 
-{/* This is main */}
-	  <Grid item xs={12} >
-		<Box sx={{
-        flexGrow: 1,  height: "100vh", width: "100vw", margin:0, padding:0,
-        background: 'linear-gradient(to right, #EA4224 0%, #EDC24F 50%, #EA4224 100%)'
-      }}
-	  >
-		<Box display="flex" justifyContent="center" alignItems="center"
-			sx={{height:"100vh"}}>
-        		<img src={calm} alt="calmScorpion" 
-				style={{ objectFit:"cover", width:"100%", height:"100%", opacity:"0.4"}}></img>
+				{/* This is navigation */}
+				<Grid item xs={12} >
+					<Box> 
+						<AppBar position="fixed" style={{ backgroundColor: "#09090C", height:'6vh' }}>
+							<Toolbar>
+								<Grid container justifyContent="center" alignItems="center">
+
+									<Grid item xs={3} >
+										<Box  component="img" alt="Logo" src={logo} sx={{height: 57}}></Box>
+									</Grid>
+
+									<Grid item xs={3} >
+										<Button variant="contained"
+											sx={{ background: 'linear-gradient(to right, #EA4224 0%, #EDC24F 50%, #EA4224 100%)', color: '#000000'}}>
+											Main
+										</Button>
+									</Grid>
+
+									<Grid item xs={3} >
+										<h2>Master of Pong</h2>
+									</Grid>
+
+									<Grid item xs={3} >
+										<Button variant="contained"
+											sx={{background: 'linear-gradient(to right, #EA4224 0%, #EDC24F 50%, #EA4224 100%)', color: '#000000'}}>
+											Login
+										</Button>
+									</Grid>
+								</Grid>
+							</Toolbar>
+						</AppBar>
+					</Box> 
+				</Grid>
+
+
+				{/* This is main */}
+				<Grid item xs={12} >
+					<Box sx={{ flexGrow: 1, background: 'linear-gradient(to right, #EA4224 0%, #EDC24F 50%, #EA4224 100%)' }}>
+						<Box display="flex" justifyContent="center" alignItems="center" sx={{height:"100vh"}}>
+							<img src={calm} alt="calmScorpion" style={{ objectFit:"cover", width:"100%", height:"100%", opacity:"0.4"}}></img>
+						</Box>
+					</Box>
+				</Grid>
+
+
+
+
+				{/* This is footer */}
+				<Grid item xs={12}>
+					<Box > 
+						<AppBar position="fixed"
+							style={{ backgroundColor: "#09090C",  height:'6vh'}}
+							sx={{ top: "auto", bottom: 0 }}>
+							<Toolbar>
+								<Grid container spacing={12} justifyContent="center" alignItems="center">
+									<Grid item md={12}>
+									<p>Footer</p>
+									</Grid>
+								</Grid>
+							</Toolbar>
+						</AppBar>
+					</Box> 
+				</Grid>
+
+
+			</Grid>
 		</Box>
-
-		</Box>
-	  </Grid>
-
-
-
-
-{/* This is footer */}
-	  <Grid item xs={12}>
-	<Box > 
-			<AppBar position="fixed"
-  				style={{ backgroundColor: "#09090C",  height:'6vh'}}
-  				sx={{ top: "auto", bottom: 0 }}>
-  				<Toolbar>
-					<Grid container spacing={12} justifyContent="center" alignItems="center">
-	  					<Grid item md={12}>
-							<p>Footer</p>
-	  					</Grid>
-					</Grid>
-  				</Toolbar>
-			</AppBar>
-	</Box> 
-	  </Grid>
-
-
-	</Grid>
-  </Box>
-
-  );
+	);
 };
-
-
 
 export default App;
 
