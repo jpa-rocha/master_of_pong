775dbab6fe9f31730cc6fbc475046a1074e83ab2
GulenayMer <mervegulenay@gmail.com>
2023-05-21 18:40:10 +0200

    fetch data exp.

--- frontend/app/src/components/Login.tsx
+++ frontend/app/src/components/Login.tsx
@@ -8,13 +8,30 @@ This is the Login Page */
 
 const Login: React.FunctionComponent = () => {
 
-	
 	const navigate = useNavigate();
+	
+		/* 
+		example :
+		https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-064a71c5c3a2b98669be43b3e76df81d56f590615ee56de4481638789ad90afd&redirect_uri=https%3A%2F%2F127.0.0.1%3A3000%2Fmain&response_type=code
+		 */
 
+	/* TODO: Get clinetID 	
+	const redirectURI = "..../main";
+		const clinentID = ""; */
+	
 	function handleSubmit(e: React.FormEvent) {
 		e.preventDefault();
+
 		navigate('/main');
-	  };
+
+		/*  TODO: instead of navigate('/main), we need 42 API : */
+
+		/* const apiURL = `https://api.intra.42.fr/oauth/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=code`;
+		window.location.href = apiURL; 
+	
+		*/
+	};
+
 
 	return (
 
