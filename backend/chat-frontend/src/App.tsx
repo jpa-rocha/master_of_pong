c44c8ed0a931db5de9b1f64b9a9f45448814a6ef
libacchu <libacchu@students.42wolfsburg.de>
2023-05-25 16:47:14 +0000

    chat: prints when a user connects and disconnects

--- backend/chat-frontend/src/App.tsx
+++ backend/chat-frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
-import "./App.css";
 import io, { Socket } from "socket.io-client";
 import MessageInput from "./MessageInput";
 import Message from "./Messages";
+import "./App.css";
+
+// const socket = io("http://localhost:8001");
 
 function App() {
   const [socket, setSocket] = useState<Socket>();
@@ -12,14 +14,14 @@ function App() {
     socket?.emit("message", message);
   };
 
+  const handleNewMessage = (newMessage: string) => {
+    setMessages([...messages, newMessage]);
+  };
+
   useEffect(() => {
     const newSocket = io("http://localhost:8001");
     setSocket(newSocket);
   }, [setSocket]);
-
-  const handleNewMessage = (newMessage: string) => {
-    setMessages([...messages, newMessage]);
-  };
 
   useEffect(() => {
     socket?.on("message", handleNewMessage);
@@ -28,9 +30,35 @@ function App() {
     };
   }, [handleNewMessage]);
 
+  // useEffect(() => {
+  //   socket?.on("message", handleNewMessage);
+  //   return () => {
+  //     socket?.off("message", handleNewMessage);
+  //   };
+  // }, [handleNewMessage]);
+
+  /*   
+    // Use connected with ID
+    useEffect(() => {
+    socket?.on("user connected", (userId: string) => {
+      setMessages([...messages, `User ${userId} connected`]);
+    });
+    socket?.on("user disconnected", (userId: string) => {
+      setMessages([...messages, `User ${userId} disconnected`]);
+    });
+  }, [messages]); */
+
+  useEffect(() => {
+    socket?.on("user connected", () => {
+      setMessages([...messages, `User connected`]);
+    });
+    socket?.on("user disconnected", () => {
+      setMessages([...messages, `User disconnected`]);
+    });
+  }, [messages]);
+
   return (
     <>
-      {" "}
       <MessageInput send={sendMessage} />
       <Message messages={messages} />
     </>
--- backend/chat-frontend/src/MessageInput.tsx
+++ backend/chat-frontend/src/MessageInput.tsx
@@ -6,11 +6,28 @@ export default function MessageInput({
   send: (value: string) => void;
 }) {
   const [value, setValue] = React.useState("");
+  const handleSend = () => {
+    send(value);
+    setValue("");
+  };
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSend();
+    }
+  };
   return (
     <>
       <div id="form">
-        <input id="input" onChange={(e)=>setValue(e.target.value)} placeholder="Type your message ..." value={value} />
-        <button className="button" onClick={() => send(value)}>Send</button>
+        <input
+          id="input"
+          onChange={(e) => setValue(e.target.value)}
+          onKeyDown={handleKeyDown}
+          placeholder="Type your message ..."
+          value={value}
+        />
+        <button className="button" onClick={handleSend}>
+          Send
+        </button>
       </div>
     </>
   );
