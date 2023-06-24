79bf97ef071436d1e0eb355a599faa04badcd5f2
libacchu <libacchu@students.42wolfsburg.de>
2023-06-24 11:04:15 +0200

    nest-api

--- backend/nest-user-api/src/main.ts
+++ backend/nest-user-api/src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsOptions } from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors();
-
-  await app.listen(3333);
+  await app.listen(3000);
 }
 bootstrap();
---
+++ backend/nest-user-api/src/users/users.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Not, Repository } from 'typeorm';
+import { type } from 'os';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  create(createUserDto: CreateUserDto) {
+    const newUser = this.usersRepository.create(createUserDto);
+
+    return this.usersRepository.save(newUser);
+  }
+
+  findAll() {
+    return this.usersRepository.find();
+  }
+
+  findOne(id: number) {
+    const user = this.usersRepository.findOneBy({ id });
+    return user;
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = await this.findOne(id);
+    return this.usersRepository.save({ ...user, ...updateUserDto });
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    return this.usersRepository.remove(user);
+  }
+}
