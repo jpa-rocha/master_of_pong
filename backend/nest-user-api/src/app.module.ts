33a10b2497bba39b8049b1d3f687b5ff0da0f275
libacchu <libacchu@students.42wolfsburg.de>
2023-06-27 09:48:49 +0200

    Authentification - We are now able to get Users information from 42 API

--- backend/nest-user-api/src/app.module.ts
+++ backend/nest-user-api/src/app.module.ts
@@ -3,20 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
-import { GameModule } from './game/game.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    database: 'user',
-    username: 'user',
-    password: 'password',
-    host: 'localhost',
-    port: 5432,
-    entities: ['dist/**/*.entity{.ts,.js}'],
-    synchronize: true,
-
-  }), UsersModule, GameModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: '--> secret here', //TODO: add secret
+      username: '--> secret here', //TODO: add secret
+      password: '--> secret here', //TODO: add secret
+      host: 'localhost',
+      port: 5432,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
