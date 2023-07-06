3857ba0ecfd5f70459a9aaf966425d05b2a1922d
libacchu <libacchu@students.42wolfsburg.de>
2023-07-06 10:38:02 +0200

    Remove unused backend apps - "pongchat"

--- backend/pong_chat/dist/game/movement.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MovementDto = void 0;
-class MovementDto {
-}
-exports.MovementDto = MovementDto;
-//# sourceMappingURL=movement.dto.js.map
