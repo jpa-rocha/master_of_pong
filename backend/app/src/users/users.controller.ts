abac0b626abdd4a099d08b16d1e9067714481cd5
libacchu <libacchu@students.42wolfsburg.de>
2023-07-11 15:21:22 +0200

    added: Post request for uploading profile photo

--- backend/app/src/users/users.controller.ts
+++ backend/app/src/users/users.controller.ts
@@ -1,7 +1,35 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import * as path from 'path';
+import { of } from 'rxjs';
+
+const storage = {
+  storage: diskStorage({
+    destination: './src/uploads/avatars',
+    filename: (req, file, cb) => {
+      const filename: string =
+        path.parse(file.originalname).name.replace(/\s/g, '') + uuidv4();
+      const extension: string = path.parse(file.originalname).ext;
+
+      cb(null, `${filename}${extension}`);
+    },
+  }),
+};
 
 @Controller('users')
 export class UsersController {
@@ -30,5 +58,15 @@ export class UsersController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
+  }
+
+  @Post('upload/:id')
+  @UseInterceptors(FileInterceptor('file', storage))
+  uploadFile(@UploadedFile() file: Express.Multer.File, @Param('id') id: string) {
+    console.log('-------------------------------');
+    console.log(file);
+    console.log('-------------------------------');
+    console.log(+id);
+    return of({ imagePath: file.filename });
   }
 }
