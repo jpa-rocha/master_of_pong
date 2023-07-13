b125060db16ec85fec7ed92ed73db5b8aa93d9be
jrocha <janos.rocha@gmail.com>
2023-07-13 17:38:23 +0200

    added protection to routes

--- frontend/app/src/index.tsx
+++ frontend/app/src/index.tsx
@@ -9,13 +9,14 @@ import Game from "./components/GameCanvas/Game";
 import ProfilePage from "./components/Profile/Profile";
 import GetUserName from './components/ChatPage/GetUserName';
 import ChatPage from './components/ChatPage/ChatPage';
-import * as socketIO  from "socket.io-client"; 
+import * as socketIO  from "socket.io-client";
 import { Socket } from 'socket.io-client';
 import UserProfile from './components/UserProfile/UserProfile';
+import PrivateRoutes from './utils/PrivateRoutes';
 
-// const URI = 'http://localhost:4000';
+const URI = 'http://localhost:4000';
 
-// const socket: Socket = socketIO.connect(URI);
+const socket: Socket = socketIO.connect(URI);
 // @ts-ignore
 
 
@@ -26,14 +27,16 @@ root.render(
 	<React.StrictMode>
 	<BrowserRouter>
 		<Routes>
+			<Route element={<PrivateRoutes />}>
+				<Route path="/home" element={<LandingPage/>} />
+				<Route path="/game" element={<Game/>} />
+				<Route path="/main" element={<MainPage/>} />
+				{/* <Route path="/profile" element={<UserProfile userName={'Bob'} image={'./src/images/Profile/default_profile_image.jpg'} friends={[]}/>} /> */}
+				<Route path="/profile" element={<ProfilePage/>} />
+				<Route path="/chat" element={<GetUserName socket={socket}/> }/>
+				<Route path="/chatPage" element={<ChatPage socket={socket}/> }/>
+			</Route>
 			<Route path="/" element={<App/>} />
-			<Route path="/home" element={<LandingPage/>} />
-			<Route path="/game" element={<Game/>} />
-			<Route path="/main" element={<MainPage/>} />
-			{/* <Route path="/profile" element={<UserProfile userName={'Bob'} image={'./src/images/Profile/default_profile_image.jpg'} friends={[]}/>} /> */}
-			<Route path="/profile" element={<ProfilePage/>} />
-			{/* <Route path="/chat" element={<GetUserName socket={socket}/> }/>
-			<Route path="/chatPage" element={<ChatPage socket={socket}/> }/> */}
 		</Routes>
 	</BrowserRouter>
 	 </React.StrictMode>
