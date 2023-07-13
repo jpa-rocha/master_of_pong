fc03b7c0827177495e70aaa272fc4e515bbc684a
libacchu <libacchu@students.42wolfsburg.de>
2023-07-13 19:03:21 +0200

    Added the navigation to NavBar Main
    Co-authored-by: Merve G. <GulenayMer@users.noreply.github.com>

--- frontend/app/src/components/GameCanvas/Game.tsx
+++ frontend/app/src/components/GameCanvas/Game.tsx
@@ -1,10 +1,11 @@
 import { Grid, Box } from "@mui/material";
-import calm from "../../images/CalmScorpion.gif"
+import calm from "../../images/CalmScorpion.gif";
 //import Footer from "./Footer";
 import NavBarLandingPage from "../NavBarLandingPage";
 //import Chat from './Messages/Chat'
 //import ChatOnGame from "./ChatOnGamePage/ChatOnGame";
-import GameCanvas from './GameCanvas';
+import GameCanvas from "./GameCanvas";
+import NavBarMainPage from "../NavBarMainPage";
 
 /* import * as socketIO  from "socket.io-client"; 
 import { Socket } from 'socket.io-client';
@@ -14,55 +15,81 @@ const URI = 'http://localhost:4000';
 const socket: Socket = socketIO.connect(URI); */
 
 const Game = () => {
+  return (
+    <>
+      <Grid container>
+        {/* This is navigation */}
 
-	return (
-	<>
-		<Grid container>
-		{/* This is navigation */}
+        <Grid item xs={12}>
+          <NavBarMainPage></NavBarMainPage>
+        </Grid>
 
-			<Grid item xs={12} >
-				<NavBarLandingPage></NavBarLandingPage>
-			</Grid>
+        {/* This is main */}
+        <Grid item xs={12}>
+          <Box
+            sx={{
+              flexGrow: 1,
+              background:
+                "linear-gradient(to right, #EA4224 0%, #EDC24F 50%, #EA4224 100%)",
+            }}
+          >
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              sx={{ height: "100vh", position: "relative" }}
+            >
+              <img
+                src={calm}
+                alt="calmScorpion"
+                style={{
+                  objectFit: "cover",
+                  width: "100%",
+                  height: "100%",
+                  opacity: "0.4",
+                }}
+              ></img>
 
-		{/* This is main */}
-		<Grid item xs={12}>
-			<Box sx={{ flexGrow: 1, background: 'linear-gradient(to right, #EA4224 0%, #EDC24F 50%, #EA4224 100%)' }}>
-				<Box display="flex" justifyContent="center" alignItems="center" sx={{ height: '100vh', position: 'relative' }}>
-					<img src={calm} alt="calmScorpion" style={{ objectFit: 'cover', width: '100%', height: '100%', opacity: '0.4' }}></img>
-
-					<div
-					style={{
-						display: 'flex',
-						position: 'absolute',
-						top: 0,
-						left: 0,
-						right: 0,
-						bottom: 0,
-						flexDirection: 'row',
-					}}
-					>
-					{/* <div style={{ flex: 1, display: 'flex' }}>
+              <div
+                style={{
+                  display: "flex",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  flexDirection: "row",
+                }}
+              >
+                {/* <div style={{ flex: 1, display: 'flex' }}>
 						<Chat />
 					</div> */}
-					{/* <div style={{ flex: 1, display: 'flex'}}>
+                {/* <div style={{ flex: 1, display: 'flex'}}>
 						<ChatOnGame socket={socket} />
 					</div> */}
-					
-					<div style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-						<GameCanvas/>
-					</div>
-					</div>
-				</Box>
-			</Box>
-		</Grid>
 
-		{/* This is footer */}
-		{/* 	<Grid item xs={12}>
+                <div
+                  style={{
+                    flex: 1,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <GameCanvas />
+                </div>
+              </div>
+            </Box>
+          </Box>
+        </Grid>
+
+        {/* This is footer */}
+        {/* 	<Grid item xs={12}>
 				<Footer></Footer>
 			</Grid> */}
-		</Grid>
-	</>
-	)
-}
+      </Grid>
+    </>
+  );
+};
 
 export default Game;
--- frontend/app/src/index.tsx
+++ frontend/app/src/index.tsx
@@ -1,45 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import MainPage from "./components/MainPage";
 import Game from "./components/GameCanvas/Game";
 import ProfilePage from "./components/Profile/Profile";
-import GetUserName from './components/ChatPage/GetUserName';
-import ChatPage from './components/ChatPage/ChatPage';
-import * as socketIO  from "socket.io-client";
-import { Socket } from 'socket.io-client';
-import UserProfile from './components/UserProfile/UserProfile';
-import PrivateRoutes from './utils/PrivateRoutes';
+import GetUserName from "./components/ChatPage/GetUserName";
+import ChatPage from "./components/ChatPage/ChatPage";
+import * as socketIO from "socket.io-client";
+import { Socket } from "socket.io-client";
+import UserProfile from "./components/UserProfile/UserProfile";
+import PrivateRoutes from "./utils/PrivateRoutes";
 
-const URI = 'http://localhost:4000';
+const URI = "http://localhost:4000";
 
 const socket: Socket = socketIO.connect(URI);
 // @ts-ignore
 
-
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-	<React.StrictMode>
-	<BrowserRouter>
-		<Routes>
-			<Route element={<PrivateRoutes />}>
-				<Route path="/home" element={<LandingPage/>} />
-				<Route path="/game" element={<Game/>} />
-				<Route path="/main" element={<MainPage/>} />
-				{/* <Route path="/profile" element={<UserProfile userName={'Bob'} image={'./src/images/Profile/default_profile_image.jpg'} friends={[]}/>} /> */}
-				<Route path="/profile" element={<ProfilePage/>} />
-				<Route path="/chat" element={<GetUserName socket={socket}/> }/>
-				<Route path="/chatPage" element={<ChatPage socket={socket}/> }/>
-			</Route>
-			<Route path="/" element={<App/>} />
-		</Routes>
-	</BrowserRouter>
-	 </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<PrivateRoutes />}>
+          <Route path="/game" element={<Game />} />
+          <Route path="/main" element={<MainPage />} />
+          {/* <Route path="/profile" element={<UserProfile userName={'Bob'} image={'./src/images/Profile/default_profile_image.jpg'} friends={[]}/>} /> */}
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/chat" element={<GetUserName socket={socket} />} />
+          <Route path="/chatPage" element={<ChatPage socket={socket} />} />
+        </Route>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
-
-
