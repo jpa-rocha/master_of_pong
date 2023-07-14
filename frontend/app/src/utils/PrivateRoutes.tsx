b5b9ad6d84bd19280788d9340e67c4d8a4a62038
libacchu <libacchu@students.42wolfsburg.de>
2023-07-14 11:29:40 +0200

    Frontend: get JWT token from cookie

--- frontend/app/src/utils/PrivateRoutes.tsx
+++ frontend/app/src/utils/PrivateRoutes.tsx
@@ -1,10 +1,30 @@
-import { Outlet, Navigate } from 'react-router-dom'
+import { Outlet, Navigate } from "react-router-dom";
 
 const PrivateRoutes = () => {
-    let auth = {'token':true}
-    return(
-        auth.token ? <Outlet/> : <Navigate to="/"/>
-    )
+  let token = getToken("jwtToken");
+  console.log("token : " + token);
+  let auth = { token: true };
+  return auth.token ? <Outlet /> : <Navigate to="/" />;
+};
+
+export default PrivateRoutes;
+
+function getToken(tokenName: string): string | null {
+  const cookies = document.cookie.split(";");
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim();
+    if (cookie.startsWith(tokenName + "=")) {
+      return cookie.substring(tokenName.length + 1);
+    }
+  }
+  return null;
 }
 
-export default PrivateRoutes
+// function isTokenValid(token: string, secrect: string): boolean {
+//     try {
+//         jwt.verify(token, secrect);
+//         return true;
+//     } catch (err) {
+//         return false;
+//     }
+// }
