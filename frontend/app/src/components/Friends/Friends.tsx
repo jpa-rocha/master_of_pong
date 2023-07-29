93a5dd6da1cf2bae460bf76297fc8e333974bee7
GulenayMer <mervegulenay@gmail.com>
2023-07-30 00:01:25 +0200

    friends

--- frontend/app/src/components/Friends/Friends.tsx
+++ frontend/app/src/components/Friends/Friends.tsx
@@ -6,14 +6,14 @@
 		- display if a user is already a friend
 
 */
-import { Box, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import "./FriendsStyle.css";
 import NavBarMainPage from "../Navigation/NavBarMainPage";
 import Footer from "../Footer";
 import { Socket } from "socket.io-client";
 import { getUserID, getToken } from "../../utils/Utils";
+//import UserTable from './UserTable'
 
 interface UserProps {
   username: string;
@@ -77,44 +77,81 @@ const FriendsPage: React.FunctionComponent<FriendsPageProps> = ({ socket }) => {
 
   return (
     <>
-      <Grid container direction="column">
+    <Grid container direction="column">
         <Grid item xs={2}>
-          <NavBarMainPage></NavBarMainPage>
+        	<NavBarMainPage></NavBarMainPage>
         </Grid>
 
-        <Grid item xs={10} mt={9}>
-          <Box className="Search">
-            <input
-              value={input}
-              type="text"
-              placeholder="Search for a user"
-              onChange={handleSearchChange}
-            />
-          </Box>
-          <ul>
-            {users &&
-              users.map((item, index) => (
-                <Box
-                  className="User"
-                  key={index}
-                  display="flex"
-                  alignItems="center"
-                >
-                  <li>{item.username}</li>
-                  <Box flex="1" />
-                  <li>{item.status}</li>
-                  <Box flex="2" />
-                  {!item.isFriend ? (
-                    <button onClick={() => handleSendFriendRequest(item.id)}>
-                      Add Friend
-                    </button>
-                  ) : (
-                    <span>Already a friend</span>
-                  )}
-                </Box>
-              ))}
-          </ul>
-        </Grid>
+		<Grid item xs={10} mt={9}>
+			<div className="relative overflow-x-auto shadow-md sm:rounded-lg md:px-10">
+    			<div className="flex items-center justify-end pb-4 bg-white">
+				{/* For the search bar */}
+        			<div className="relative">
+        				<div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        			    	<svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" 
+								fill="none"
+        			    		viewBox="0 0 20 20">
+        			    		<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+        			        			d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+        			    	</svg>
+        			</div>
+        			<input type="text" placeholder="Search for users"
+            			className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 
+									focus:ring-blue-500 focus:border-blue-500"
+						onChange={handleSearchChange}/>
+        		</div>
+    			</div>
+				{users && users.map( (item, index) => (
+				<table key={index} className="w-full text-sm text-left text-gray-500 pb-4">
+					<thead className="text-xs text-gray-700 uppercase bg-gray-50">
+						<tr>
+							<th scope="col" className="px-6 py-3"></th>
+							<th scope="col" className="px-6 py-3">User</th>
+							<th scope="col" className="px-6 py-3">Status</th>
+							<th scope="col" className="px-6 py-3">Add</th>
+							<th scope="col" className="px-6 py-3">Remove</th>
+							<th scope="col" className="px-6 py-3"></th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr className="bg-white border-b hover:bg-gray-50">
+							<th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap">
+								<img className="w-10 h-10 rounded-full" src="" alt="user"/>
+								<div className="pl-3">
+									<div className="text-base font-semibold">{item.username}</div>
+									<div className="font-normal text-gray-500"></div>
+								</div>  
+							</th>
+							<td className="px-6 py-4">Friend</td>
+							<td className="px-6 py-4">
+								<div className="flex items-center">
+									<div className="h-2.5 w-2.5 rounded-full bg-green-500 mr-2"></div>
+										{item.status}
+									</div>
+							</td>
+							<td className="px-6 py-4">
+								{ !item.isFriend ? (
+									<button className="font-medium text-blue-600 hover:underline">
+									Add as Friend
+									</button>
+								) : (
+									<span>Already a friend</span>
+								)}
+							</td>
+							<td className="px-6 py-4">
+								{item.isFriend ? (
+									<button className="font-medium text-blue-600 hover:underline">
+										Remove Friend
+									</button>	
+								) : (
+									<span>Not a friend</span>
+								)}
+							</td>
+						</tr>
+					</tbody>
+				</table>))}
+    		</div>
+		</Grid>
 
         <Grid item xs={12}>
           <Footer></Footer>
