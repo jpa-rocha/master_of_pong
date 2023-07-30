bcb71f1ac6c07b0104e28dd3c7aef19d7245079f
GulenayMer <mervegulenay@gmail.com>
2023-07-30 19:09:30 +0200

    chat

--- frontend/app/src/components/ChatPage/ChatBar.tsx
+++ frontend/app/src/components/ChatPage/ChatBar.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Socket } from "socket.io-client";
-import "./chatPageStyle/chat.css";
 import axios from "axios";
 import { getToken } from "../../utils/Utils";
 
@@ -89,48 +88,69 @@ const ChatBar: React.FunctionComponent<ChatBarProps> = ({ socket }) => {
   }
 
   return (
-    <>
-      <div className="chatSidebar">
-        <h2>Chat Bar</h2>
-        <div className="activeUsers">
-          <h4 className="chatHeader">Friends</h4>
-          <div className="onlineUsers">
-            <ul>
-              {users.map((user) => (
-                <div key={user.username}>
-                  {user.isFriend ? (
-                    <button onClick={() => handleGetChat(user)}>
-                      {user.username} {user.status}
-                    </button>
-                  ) : null}
-                </div>
-              ))}
-            </ul>
-          </div>
-        </div>
-
-        <div className="allUsers">
-          <h4 className="chatHeader">Chat Rooms</h4>
-          <button onClick={() => createChatRoom()}>
-            create chatRoom
-          </button>
-          <button onClick={() => joinChatRoom()}>
-            join chatRoom
-          </button>
-          <div className="chatUsers">
-            <ul>
-              {chatRooms && chatRooms.map((chat) => (
-                <div key={chat.id}>
-                    <button onClick={() => getChatRoomMessages(chat.id)}>
-                      {chat.title}
-                    </button>
-                </div>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
-    </>
+	<div className="flex flex-col py-8 pl-6 pr-2 mt-3 rounded-2xl w-64 bg-gray-100 flex-shrink-0">
+	<div className="flex flex-row items-center justify-start h-3 w-full">
+	  <div className="flex items-center justify-center rounded-2xl text-white bg-gray-800 h-10 w-10">
+		<svg
+		  className="w-6 h-6"
+		  fill="none"
+		  stroke="currentColor"
+		  viewBox="0 0 24 24"
+		  xmlns="http://www.w3.org/2000/svg"
+		>
+		  <path
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth={2}
+			d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
+		  />
+		</svg>
+	  </div>
+	  <div className="ml-2 font-bold text-2xl">Chat</div>
+	</div>
+  
+	<div className="flex flex-col mt-8">
+	  <div className="flex flex-row items-center justify-between text-xs">
+		<span className="font-bold">Friends</span>
+	  </div>
+		  {users.map((user) => (
+		  <div key={user.username} className="flex flex-col space-y-1 mt-4 -mx-2 h-48 overflow-y-auto">
+			{user.isFriend ? (
+		   <button onClick={() => handleGetChat(user)}
+		   className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2">
+			 <div className="ml-2 text-sm font-semibold">{user.username} {user.status} </div>
+		   </button>
+		   ) : null}
+		   </div>
+		))}
+	 
+	</div>
+	<div className="flex flex-col mt-2">
+	  <div className="flex flex-row items-center justify-between text-xs">
+		<span className="font-bold">Chat Rooms</span>
+	  </div>
+	  <div className="flex flex-col space-y-1 mt-2 mx-2 h-48 overflow-y-auto">
+		<button  onClick={() => createChatRoom()}
+		 className="flex flex-row items-center hover:bg-gray-100">
+		create chatRoom
+		</button>
+		<button onClick={() => joinChatRoom()} 
+			className="flex flex-row items-center hover:bg-gray-100">
+		join chatRoom
+		</button>
+		<ul>
+		{chatRooms && chatRooms.map((chat) => (
+		  <div key={chat.id}>
+			  <button onClick={() => getChatRoomMessages(chat.id)}>
+				{chat.title}
+			  </button>
+		  </div>
+		))}
+	  </ul>
+	  </div>
+	</div>
+</div>
+    
   );
 };
 
---
+++ frontend/app/src/components/ChatPage/testPages/ChatExample.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ChatBarTest from './ChatBarTest';
+import ChatBodyTest from './ChatBodyTest';
+import ChatFooterTest from './ChatFooterTest';
+
+const ChatExample: React.FC = () => {
+  return (
+    <div className="flex h-screen text-gray-800 md-p-20 "> 
+      <div className="flex h-4/5 flex-row w-full ">
+       <ChatBarTest></ChatBarTest>
+
+        <div className="flex flex-col flex-auto h-full p-6">
+          <div className="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-gray-100 h-full p-4">
+		<ChatBodyTest></ChatBodyTest>
+		<ChatFooterTest></ChatFooterTest>
+        
+          </div>
+        </div>
+      </div>
+   </div> 
+  );
+};
+
+export default ChatExample;
