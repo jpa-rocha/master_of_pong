6b9c0eb0437b554c37b0478e71dddc2f4d4879fd
GulenayMer <mervegulenay@gmail.com>
2023-08-05 14:40:27 +0200

    land.page

--- frontend/app/src/components/LandingPage.tsx
+++ frontend/app/src/components/LandingPage.tsx
@@ -1,30 +1,32 @@
-import NavBarLandingPage from "./Navigation/NavBarLandingPage";
-import calm from "../images/CalmScorpion.gif";
-import Footer from "./Footer";
-import { Grid } from "@mui/material"
 
 const imgStyle = {
-    background: 'linear-gradient(to right, #EA4224 0%, #EDC24F 50%, #EA4224 100%)',
+    background: 'linear-gradient(to right, #EA4224 0%, #c49b2b 50%, #EA4224 100%)',
 };
+
+const  btnClass = `
+text-gray-900 text-lg uppercase 
+bg-transparent border border-gray-900 
+rounded-lg 
+px-5 py-2.5 w-60 my-5
+focus:outline-none focus:ring-4 
+focus:ring-gray-100 font-bold
+transition ease-in-out delay-110 hover:-translate-x-1
+hover:opacity-90
+hover:scale-110 hover:bg-gradient-to-br from-orange-500 via-red-200 to-yellow-200
+`
+
 
 const LandingPage: React.FunctionComponent = () => {
 	return (
     <>
-	<Grid container>
-		<Grid item xs={12}>
-			<NavBarLandingPage></NavBarLandingPage>
-		</Grid>
-		<Grid item xs={12}>
-			<div className="relative h-[100vh]"> 
-				<img className="w-full h-[100vh]" src={calm} alt="calm"></img>
-				<div className="absolute inset-0  opacity-50" style={imgStyle}></div> 
-			</div> 
-		</Grid>
-		<Grid item xs={12}>
-			<Footer></Footer>
-		</Grid>
-	</Grid>
-	
+	<div className="flex flex-col justify-center items-center h-[100vh]" style={imgStyle}> 
+		<h1 className="text-4xl my-3 text-gray-900 xl:text-8xl italic">Master of Pong</h1> 
+		<button className={btnClass}>
+			<a href="http://localhost:5000/api/auth/redirect">
+				Login
+			</a>
+	</button>
+	</div> 
     </>
 );
 };
