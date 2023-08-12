db429c73b22949f2d10ad5b89207221e89371f99
jrocha <janos.rocha@gmail.com>
2023-08-12 17:25:33 +0200

    fixed dockerfiles

---
+++ backend/app/entrypoint.py
@@ -0,0 +1,10 @@
+import subprocess as sp
+
+install = ["npm", 'install']
+run = ["npm", "run", "start:dev"]
+chmod = ['chmod', '777', '/node_modules']
+
+install_proc = sp.Popen(install)
+install_proc.wait()
+sp.run(chmod)
+sp.run(run)
