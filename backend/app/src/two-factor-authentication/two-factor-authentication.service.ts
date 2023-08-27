aab2e211a399fe5ccbf6f7e6c91dfad67c1af792
libacchu <libacchu@students.42wolfsburg.de>
2023-08-27 23:35:05 +0200

    twofa secret is now encrypted
    Co-authored-by: João Rocha <jpa-rocha@users.noreply.github.com>

--- backend/app/src/two-factor-authentication/two-factor-authentication.service.ts
+++ backend/app/src/two-factor-authentication/two-factor-authentication.service.ts
@@ -5,19 +5,13 @@ import { toFileStream } from 'qrcode';
 import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
 import { Response } from 'express';
-import {CryptoJS} from 'crypto-js/core';
-
-
-let var1 ="";
-let var2 ="";
 
 @Injectable()
 export class TwoFactorAuthenticationService {
   constructor(
     private readonly usersService: UsersService,
     private readonly configService: ConfigService,
-  ){};
-
+  ) {}
 
   public async generateTwoFactorAuthenticationSecret(user: User) {
     const secret: string = authenticator.generateSecret();
@@ -25,13 +19,13 @@ export class TwoFactorAuthenticationService {
       user.email,
       this.configService.get('POSTGRES_DB'),
       secret,
-    )
-    console.log('SECRET', secret);
-    var1 = secret;
+    );
 
-    var CryptoJS = require("crypto-js");
-    var hash = CryptoJS.AES.encrypt(secret, this.configService.get<string>('TWOFA_SECRET')).toString();
-    console.log("HAHAAAAAAAAAAAAAAAASH", hash)
+    var CryptoJS = require('crypto-js');
+    var hash = CryptoJS.AES.encrypt(
+      secret,
+      this.configService.get<string>('TWOFA_SECRET'),
+    ).toString();
     await this.usersService.setTwoFactorAuthenticationSecret(hash, user.id);
 
     return {
@@ -48,26 +42,20 @@ export class TwoFactorAuthenticationService {
     twoFactorAuthenticationCode: string,
     user: User,
   ) {
-    console.log('USER SECRET', user.twofa_secret)
+    console.log('USER SECRET', user.twofa_secret);
 
-    var CryptoJS = require("crypto-js");
-    var bytes  = CryptoJS.AES.decrypt(user.twofa_secret, this.configService.get<string>('TWOFA_SECRET'));
+    var CryptoJS = require('crypto-js');
+    var bytes = CryptoJS.AES.decrypt(
+      user.twofa_secret,
+      this.configService.get<string>('TWOFA_SECRET'),
+    );
     var twofa_secret = await bytes.toString(CryptoJS.enc.Utf8);
 
-    var2 = twofa_secret;
-
-    if(var1 == var2){
-      console.log("-----------TRUEEEEEE----------------");
-    }
-    else{
-      console.log("-----------FALSEEEEE----------------");
-    }
-    console.log("ALSO SECRET", twofa_secret)
     const verify = authenticator.verify({
       token: twoFactorAuthenticationCode,
       secret: twofa_secret,
     });
-    console.log(verify)
+    console.log(verify);
     return verify;
   }
 }
